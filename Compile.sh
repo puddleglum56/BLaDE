e015fcbb77ce918b1fee8f4469507904415fb1a6
Liam McFadden <liam.mcfadden@codexis.com>
2023-07-10 20:16:33 -0400

    Add additional lnx27 setup

--- Compile.sh
+++ Compile.sh
@@ -1,10 +1,12 @@
 #! /bin/bash -l
+export CC=/usr/local/bin/gcc
+export CXX=/usr/local/bin/g++
 
 source modules
 
 mkdir build
 cd build
-cmake -DUNITS=AKMA -DCMAKE_BUILD_TYPE=Release ../src
+/usr/local/cmake-3.24.1-linux-x86_64/bin/cmake -DUNITS=AKMA -DCMAKE_BUILD_TYPE=Release ../src
 # cmake -DUNITS=AKMA -DPRECISION=DOUBLE -DCMAKE_BUILD_TYPE=Release ../src
 
 make $1 blade
